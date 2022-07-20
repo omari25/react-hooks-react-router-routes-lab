31b2f665811a394f66aaec7615bc203079cb428f
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-07-20 21:46:19 +0300

    commit tree 1d2d8a002af250d97d48768ec499904e33b4e98e

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((data) => (
+        <div key={data.title}>
+          <h2>{data.title}</h2>
+          <p>Time: {data.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {data.genres.map((genre) =>(
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Movies;
